24d9f5c80d40df9a86a4f7a857dbcfa2965ec4bb
Dan Chung <danetix@gmail.com>
2018-06-20 00:07:46 -0400

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,12 +4,12 @@
 def key_for_min_value(name_hash)
   min_value = nil
   min_key = nil
-  name_hash.map.with_index do |(key, value), index|
-    if(index == 0)
+  name_hash.map.with_index do |(key, value), index| ## Gives us index access
+    if(index == 0) ## If first iteration, use current values for comparison
       min_value = value
       min_key = key
     end
-    if value < min_value
+    if value < min_value ## If new value is less, set it as new min value
       min_value = value
       min_key = key
     end
